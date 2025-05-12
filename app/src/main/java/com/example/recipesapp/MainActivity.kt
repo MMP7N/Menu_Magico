2f9240e4b417ad4fbd13fcc85aaaa21e82d49d3a
Marcos Medina <marmedpal2@alu.edu.gva.es>
2025-05-12 15:54:28 +0200

    Barra de navegacion creada y en funcionamiento

--- app/src/main/java/com/example/recipesapp/MainActivity.kt
+++ app/src/main/java/com/example/recipesapp/MainActivity.kt
@@ -1,20 +1,19 @@
 package com.example.recipesapp
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.btn_nav)
+        val navController = findNavController(this, R.id.host_fragment)
+
+        NavigationUI.setupWithNavController(bottomNavigationView, navController)
     }
 }
