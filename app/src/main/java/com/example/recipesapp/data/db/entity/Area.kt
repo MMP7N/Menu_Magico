abb4f987153b9f12c09004ac492a8c21229d8268
Marcos Medina <marmedpal2@alu.edu.gva.es>
2025-06-17 12:48:28 +0200

    Entities de area añadidas

---
+++ app/src/main/java/com/example/recipesapp/data/db/entity/Area.kt
@@ -0,0 +1,5 @@
+package com.example.recipesapp.data.db.entity
+
+data class Area(
+    val strArea: String
+)
---
+++ app/src/main/java/com/example/recipesapp/data/db/entity/AreaList.kt
@@ -0,0 +1,5 @@
+package com.example.recipesapp.data.db.entity
+
+data class AreaList(
+    val meals: List<Area>
+)
